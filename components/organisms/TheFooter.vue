90836808bb5eb9c3d8c36ec656618ddc31238552
Hendrik Neumann <henne@armselig.net>
2021-10-21 10:28:25 +0200

    feat(footer, header): make components functional

--- components/organisms/TheFooter.vue
+++ components/organisms/TheFooter.vue
@@ -1,5 +1,11 @@
-<template>
-  <footer>
+<template functional>
+  <footer
+    :ref="data.ref"
+    :class="[data.class, data.staticClass]"
+    :style="[data.style, data.staticStyle]"
+    v-bind="data.attrs"
+    v-on="listeners"
+  >
     <div class="container mx-auto">
       <p>Site footer</p>
     </div>
--- components/organisms/TheHeader.vue
+++ components/organisms/TheHeader.vue
@@ -1,5 +1,11 @@
-<template>
-  <header>
+<template functional>
+  <header
+    :ref="data.ref"
+    :class="[data.class, data.staticClass]"
+    :style="[data.style, data.staticStyle]"
+    v-bind="data.attrs"
+    v-on="listeners"
+  >
     <div class="container mx-auto">
       <p>Site header</p>
     </div>
